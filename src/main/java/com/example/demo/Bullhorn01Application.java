95facc5f30abe4b457064aa4ff79b3f325b056d2
Tory Lamkin <torylamkin@Torys-MacBook-Air.local>
2017-12-08 18:08:01 -0500

    Initial commit

---
+++ src/main/java/com/example/demo/Bullhorn01Application.java
@@ -0,0 +1,12 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Bullhorn01Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Bullhorn01Application.class, args);
+	}
+}
